586ce5514d005341bd29f30511767034ac80bde7
keplib <keplib@gmail.com>
2024-09-16 23:08:44 +0200

    Initial Commit

---
+++ assignment1/assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  assignment1App.swift
+//  assignment1
+//
+//  Created by Balázs Képli on 16/9/24.
+//
+
+import SwiftUI
+
+@main
+struct assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
