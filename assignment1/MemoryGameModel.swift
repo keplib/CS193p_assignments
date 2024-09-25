60950c3dfc67dc0defc79fdd8bf14bb660736c5b
keplib <keplib@gmail.com>
2024-09-25 18:31:25 +0200

    assignement2: add model and VM

---
+++ assignment1/MemoryGameModel.swift
@@ -0,0 +1,8 @@
+//
+//  MemoryGameModel.swift
+//  assignment1
+//
+//  Created by Balázs Képli on 25/9/24.
+//
+
+import Foundation
---
+++ assignment1/MemoryGameVM.swift
@@ -0,0 +1,8 @@
+//
+//  MemoryGameVM.swift
+//  assignment1
+//
+//  Created by Balázs Képli on 25/9/24.
+//
+
+import Foundation
